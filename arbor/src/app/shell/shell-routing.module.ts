2c2203d14c28b3f8dbd427b3a88070860292c271
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-02 16:17:57 +0300

    added modules for admin-panel and main page

---
+++ arbor/src/app/shell/shell-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'admin-panel',
+      },
+      {
+        path: 'bd',
+        pathMatch: 'full',
+        loadChildren: '../main-page/main-page.module#MainPageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ShellRoutingModule { }
