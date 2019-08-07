96a9c9199d2fa4b87c10b70223943ed7b34a729b
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-07 22:31:39 +0300

    added anchor links in header

--- arbor/src/app/app-routing.module.ts
+++ arbor/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, { useHash: false, anchorScrolling: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
