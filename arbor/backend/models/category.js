d84a5eb613d3ea918f0f0294aa31f38b0d2973f6
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-07 14:49:16 +0300

    added models for posts and categories

---
+++ arbor/backend/models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true},
+  slug: {
+    type: String,
+    unique: true,
+    required: true},
+  description: { type: String }
+});
+
+module.exports = mongoose.model('Category', categorySchema);
---
+++ arbor/backend/models/post.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Category = require('../models/category')
+
+const postSchema = new Schema({
+  title: { type: String },
+  body: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  owner: { type: Schema.Types.ObjectId, ref: 'User'},
+  status: {
+    type: String,
+    enum: ['published', 'draft'],
+    required: true,
+    default: 'published'
+  },
+  url: { type: String }},
+  { timestamps: true }
+);
+
+//** Get posts by category slug */
+postSchema.statics.findByCatSlug = function (slug, callback) {
+  var query = this.find()
+  Category.findOne({'slug': slug}, function (error, categories) {
+    query.where(
+      {category: categories._id}
+    ).exec(callback);
+  })
+  return query
+}
+
+module.exports = mongoose.model('Post', postSchema);
