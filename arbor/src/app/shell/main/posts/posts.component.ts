bfb4fcec6b978bec97e153eb431b54ccfaa975e5
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-08 11:43:37 +0300

    gallery stylization

--- arbor/src/app/shell/main/posts/posts.component.ts
+++ arbor/src/app/shell/main/posts/posts.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PostsComponent implements OnInit {
-
+  @HostBinding('class') classes = 'category_content';
   constructor() { }
 
   ngOnInit() {
