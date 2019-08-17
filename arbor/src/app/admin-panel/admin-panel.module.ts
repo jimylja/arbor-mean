7409858d08ef1596be001cf47a69d329f3532922
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-17 18:16:18 +0300

    added Angular Material

--- arbor/src/app/admin-panel/admin-panel.module.ts
+++ arbor/src/app/admin-panel/admin-panel.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 
 @NgModule({
   declarations: [AdminPanelComponent],
   imports: [
     CommonModule,
-    AdminPanelRoutingModule
+    AdminPanelRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class AdminPanelModule { }
